81f1ad086b17bb16cfd294025f492a26045df5d0
mihay12 <mihailgaravsyuk@gmail.com>
2020-05-12 21:30:43 +0300

    Simple code

--- js/index.js
+++ js/index.js
@@ -1,39 +1,41 @@
-const currencyEl_one = document.getElementById('currency-one');
-const amountEl_one = document.getElementById('amount-one');
-const currencyEl_two = document.getElementById('currency-two');
-const amountEl_two = document.getElementById('amount-two');
+const days = document.querySelector('.days');
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+const countdown = document.querySelector('.countdown');
+const year = document.querySelector('.year');
+const loading = document.querySelector('.loading');
 
-const rateEl = document.getElementById('rate');
-const swap = document.getElementById('swap');
+const currentYear = new Date().getFullYear();
 
-// Fetch exchange rates and update the DOM
-function calculate() {
-  const currency_one = currencyEl_one.value;
-  const currency_two = currencyEl_two.value;
+const newYearTime = new Date(`May 22 ${currentYear} 00:00:00`);
 
-  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-    .then(res => res.json())
-    .then(data => {
-      // console.log(data);
-      const rate = data.rates[currency_two];
+// Set background year
+year.innerText = currentYear;
 
-      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+// Update countdown time
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
 
-      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-    });
+  const w = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  // Add values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
 }
 
-// Event listeners
-currencyEl_one.addEventListener('change', calculate);
-amountEl_one.addEventListener('input', calculate);
-currencyEl_two.addEventListener('change', calculate);
-amountEl_two.addEventListener('input', calculate);
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
 
-swap.addEventListener('click', () => {
-  const temp = currencyEl_one.value;
-  currencyEl_one.value = currencyEl_two.value;
-  currencyEl_two.value = temp;
-  calculate();
-});
-
-calculate();
+// Run every second
+setInterval(updateCountdown, 1000);
